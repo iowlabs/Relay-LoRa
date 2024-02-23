61847ced2357e6fc4ede903d768f864ff6afe995
Amaru18 <ajrubilar@uc.cl>
2024-02-23 12:04:41 -0300

    try1OTA

--- firmware/Relay LoRa/src/main.cpp
+++ firmware/Relay LoRa/src/main.cpp
@@ -4,6 +4,11 @@
 #include <WiFiManager.h>
 #include "esp_timer.h"
 
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <HTTPUpdate.h>
+#include "WiFiClientSecure.h"
+
  //Estos pines fueron utilizados en la prueba de concepto realizada en placa perforada con los componentes modulares, estan sujetos a cambios en la realización de una placa
 #define RFM_CS       5
 #define RFM_RST      17
@@ -16,6 +21,34 @@ int releState = 0;
 bool wifiConnect = false;
 bool wifiPreviouslyConnected = false;
 int ledstate = 0;
+
+const char * rootCACertificate = \
+	"-----BEGIN CERTIFICATE-----\n" \
+	"MIIDrzCCApegAwIBAgIQCDvgVpBCRrGhdWrJWZHHSjANBgkqhkiG9w0BAQUFADBh\n" \
+	"MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n" \
+	"d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD\n" \
+	"QTAeFw0wNjExMTAwMDAwMDBaFw0zMTExMTAwMDAwMDBaMGExCzAJBgNVBAYTAlVT\n" \
+	"MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j\n" \
+	"b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IENBMIIBIjANBgkqhkiG\n" \
+	"9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4jvhEXLeqKTTo1eqUKKPC3eQyaKl7hLOllsB\n" \
+	"CSDMAZOnTjC3U/dDxGkAV53ijSLdhwZAAIEJzs4bg7/fzTtxRuLWZscFs3YnFo97\n" \
+	"nh6Vfe63SKMI2tavegw5BmV/Sl0fvBf4q77uKNd0f3p4mVmFaG5cIzJLv07A6Fpt\n" \
+	"43C/dxC//AH2hdmoRBBYMql1GNXRor5H4idq9Joz+EkIYIvUX7Q6hL+hqkpMfT7P\n" \
+	"T19sdl6gSzeRntwi5m3OFBqOasv+zbMUZBfHWymeMr/y7vrTC0LUq7dBMtoM1O/4\n" \
+	"gdW7jVg/tRvoSSiicNoxBN33shbyTApOB6jtSj1etX+jkMOvJwIDAQABo2MwYTAO\n" \
+	"BgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUA95QNVbR\n" \
+	"TLtm8KPiGxvDl7I90VUwHwYDVR0jBBgwFoAUA95QNVbRTLtm8KPiGxvDl7I90VUw\n" \
+	"DQYJKoZIhvcNAQEFBQADggEBAMucN6pIExIK+t1EnE9SsPTfrgT1eXkIoyQY/Esr\n" \
+	"hMAtudXH/vTBH1jLuG2cenTnmCmrEbXjcKChzUyImZOMkXDiqw8cvpOp/2PV5Adg\n" \
+	"06O/nVsJ8dWO41P0jmP6P6fbtGbfYmbW0W5BjfIttep3Sp+dWOIrWcBAI+0tKIJF\n" \
+	"PnlUkiaY4IBIqDfv8NZ5YBberOgOzW6sRBc4L0na4UU+Krk2U886UAb3LujEV0ls\n" \
+	"YSEY1QSteDwsOoBrp+uvFRTp2InBuThs4pFsiv9kuXclVzDAGySj4dzp30d8tbQk\n" \
+	"CAUw7C29C79Fv1C5qfPrmAESrciIxpg0X40KPMbp1ZWVbd4=\n" \
+	"-----END CERTIFICATE-----\n";
+
+WiFiClientSecure client;
+
+#define URL_fw_Bin "https://raw.githubusercontent.com/Iowlabs/Iguana/main/firmware/OTA_bin_file/firmware.bin"
 
 void EstadoWIFI() {
   // Si el pin StatusSW está en HIGH
@@ -106,6 +139,29 @@ void ManejoWifi(){
   }
 }
 
+void firmwareUpdate(void)
+{
+  //WiFiClientSecure client;
+  client.setCACert(rootCACertificate);
+  httpUpdate.setLedPin(LED_BUILTIN, LOW);
+  Serial.println("Updating firmware...");
+  t_httpUpdate_return ret = httpUpdate.update(client, URL_fw_Bin);
+  Serial.println("Firmware updated!");
+
+  switch (ret) {
+  case HTTP_UPDATE_FAILED:
+    Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s\n", httpUpdate.getLastError(), httpUpdate.getLastErrorString().c_str());
+    break;
+
+  case HTTP_UPDATE_NO_UPDATES:
+    Serial.println("HTTP_UPDATE_NO_UPDATES");
+    break;
+
+  case HTTP_UPDATE_OK:
+    Serial.println("HTTP_UPDATE_OK");
+    break;
+  }
+}
 
 void setup() {
 
@@ -136,6 +192,9 @@ void setup() {
 void loop() {
 
   ManejoWifi();
+  if (wifiConnect == 1 && wifiPreviouslyConnected) {
+    firmwareUpdate();
+  }
 
   // Espera a recibir un mensaje LoRa
   ReceptorLoRa();
